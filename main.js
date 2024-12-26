48431f0278bf2e9fb5e3af5af87127b062d21ad5
Plutonium-239 <plutonium.239.811@gmail.com>
2024-12-26 13:13:08 +0530

    commit 1 - already halfway there

---
+++ main.js
@@ -0,0 +1,423 @@
+let options = {
+  "autoPlay": true,
+  "background": {
+    "color": {
+      "value": "#0B021A"
+    },
+    "image": "",
+    "position": "",
+    "repeat": "",
+    "size": "",
+    "opacity": 1
+  },
+  "backgroundMask": {
+    "composite": "destination-out",
+    "cover": {
+      "opacity": 1,
+      "color": {
+        "value": ""
+      }
+    },
+    "enable": false
+  },
+  "clear": true,
+  "defaultThemes": {},
+  "delay": 0,
+  "fullScreen": {
+    "enable": true,
+    "zIndex": -1
+  },
+  "detectRetina": true,
+  "duration": 0,
+  "fpsLimit": 120,
+  "manualParticles": [],
+  "particles": {
+    "bounce": {
+      "horizontal": {
+        "value": 1
+      },
+      "vertical": {
+        "value": 1
+      }
+    },
+    "collisions": {
+      "absorb": {
+        "speed": 2
+      },
+      "bounce": {
+        "horizontal": {
+          "value": 1
+        },
+        "vertical": {
+          "value": 1
+        }
+      },
+      "enable": false,
+      "maxSpeed": 50,
+      "mode": "bounce",
+      "overlap": {
+        "enable": true,
+        "retries": 0
+      }
+    },
+    "color": {
+      "value": "#888",
+      "animation": {
+        "h": {
+          "count": 0,
+          "enable": false,
+          "speed": 1,
+          "decay": 0,
+          "delay": 0,
+          "sync": true,
+          "offset": 0
+        },
+        "s": {
+          "count": 0,
+          "enable": false,
+          "speed": 1,
+          "decay": 0,
+          "delay": 0,
+          "sync": true,
+          "offset": 0
+        },
+        "l": {
+          "count": 0,
+          "enable": false,
+          "speed": 1,
+          "decay": 0,
+          "delay": 0,
+          "sync": true,
+          "offset": 0
+        }
+      }
+    },
+    "effect": {
+      "close": true,
+      "fill": true,
+      "options": {},
+      "type": []
+    },
+    "groups": {},
+    "move": {
+      "angle": {
+        "offset": 0,
+        "value": 90
+      },
+      "attract": {
+        "distance": 200,
+        "enable": false,
+        "rotate": {
+          "x": 3000,
+          "y": 3000
+        }
+      },
+      "center": {
+        "x": 50,
+        "y": 50,
+        "mode": "percent",
+        "radius": 0
+      },
+      "decay": 0,
+      "distance": {},
+      "direction": "none",
+      "drift": 0,
+      "enable": true,
+      "gravity": {
+        "acceleration": 9.81,
+        "enable": false,
+        "inverse": false,
+        "maxSpeed": 50
+      },
+      "path": {
+        "clamp": false,
+        "delay": {
+          "value": 0
+        },
+        "enable": false,
+        "options": {}
+      },
+      "outModes": {
+        "default": "out",
+        "bottom": "out",
+        "left": "out",
+        "right": "out",
+        "top": "out"
+      },
+      "random": false,
+      "size": true,
+      "speed": 0.2,
+      "spin": {
+        "acceleration": 0,
+        "enable": false
+      },
+      "straight": false,
+      "trail": {
+        "enable": false,
+        "length": 11,
+        "fill": {
+          "color": {
+            "value": ""
+          }
+        }
+      },
+      "vibrate": false,
+      "warp": false
+    },
+    "number": {
+      "density": {
+        "enable": true,
+        "width": 1920,
+        "height": 1080
+      },
+      "limit": {
+        "mode": "delete",
+        "value": 0
+      },
+      "value": 300
+    },
+    "opacity": {
+      "value": {
+        "min": 0.1,
+        "max": 0.5
+      },
+      "animation": {
+        "count": 0,
+        "enable": true,
+        "speed": 0.25,
+        "decay": 0,
+        "delay": 0,
+        "sync": false,
+        "mode": "auto",
+        "startValue": "random",
+        "destroy": "none"
+      }
+    },
+    "reduceDuplicates": false,
+    "shadow": {
+      "blur": 0,
+      "color": {
+        "value": "#000"
+      },
+      "enable": false,
+      "offset": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    "shape": {
+      "close": true,
+      "fill": true,
+      "options": {},
+      "type": "circle"
+    },
+    "size": {
+      "value": 2,
+      "animation": {
+        "count": 0,
+        "enable": false,
+        "speed": 5,
+        "decay": 0,
+        "delay": 0,
+        "sync": false,
+        "mode": "auto",
+        "startValue": "random",
+        "destroy": "none"
+      }
+    },
+    "stroke": {
+      "width": 0
+    },
+    "zIndex": {
+      "value": 0,
+      "opacityRate": 1,
+      "sizeRate": 1,
+      "velocityRate": 1
+    },
+    "destroy": {
+      "bounds": {},
+      "mode": "none",
+      "split": {
+        "count": 1,
+        "factor": {
+          "value": 3
+        },
+        "rate": {
+          "value": {
+            "min": 4,
+            "max": 9
+          }
+        },
+        "sizeOffset": true,
+        "particles": {}
+      }
+    },
+    "roll": {
+      "darken": {
+        "enable": false,
+        "value": 0
+      },
+      "enable": false,
+      "enlighten": {
+        "enable": false,
+        "value": 0
+      },
+      "mode": "vertical",
+      "speed": 25
+    },
+    "tilt": {
+      "value": 0,
+      "animation": {
+        "enable": false,
+        "speed": 0,
+        "decay": 0,
+        "sync": false
+      },
+      "direction": "clockwise",
+      "enable": false
+    },
+    "twinkle": {
+      "lines": {
+        "enable": false,
+        "frequency": 0.05,
+        "opacity": 1
+      },
+      "particles": {
+        "enable": false,
+        "frequency": 0.05,
+        "opacity": 1,
+        "color": {
+          "value": ""
+        }
+      }
+    },
+    "wobble": {
+      "distance": 5,
+      "enable": false,
+      "speed": {
+        "angle": 50,
+        "move": 10
+      }
+    },
+    "life": {
+      "count": 0,
+      "delay": {
+        "value": 0,
+        "sync": false
+      },
+      "duration": {
+        "value": 0,
+        "sync": false
+      }
+    },
+    "rotate": {
+      "value": 0,
+      "animation": {
+        "enable": false,
+        "speed": 0,
+        "decay": 0,
+        "sync": false
+      },
+      "direction": "clockwise",
+      "path": false
+    },
+    "orbit": {
+      "animation": {
+        "count": 0,
+        "enable": false,
+        "speed": 1,
+        "decay": 0,
+        "delay": 0,
+        "sync": false
+      },
+      "enable": false,
+      "opacity": 1,
+      "rotation": {
+        "value": 45
+      },
+      "width": 1
+    },
+    "links": {
+      "blink": false,
+      "color": {
+        "value": "#fff"
+      },
+      "consent": false,
+      "distance": 100,
+      "enable": false,
+      "frequency": 1,
+      "opacity": 1,
+      "shadow": {
+        "blur": 5,
+        "color": {
+          "value": "#000"
+        },
+        "enable": false
+      },
+      "triangles": {
+        "enable": false,
+        "frequency": 1
+      },
+      "width": 1,
+      "warp": false
+    },
+    "repulse": {
+      "value": 0,
+      "enabled": false,
+      "distance": 1,
+      "duration": 1,
+      "factor": 1,
+      "speed": 1
+    }
+  },
+  "pauseOnBlur": true,
+  "pauseOnOutsideViewport": true,
+  "responsive": [],
+  "smooth": false,
+  "style": {},
+  "themes": [],
+  "zLayers": 100,
+  "emitters": [],
+  "motion": {
+    "disable": false,
+    "reduce": {
+      "factor": 4,
+      "value": true
+    }
+  }
+}
+
+
+tsParticles.load("particles-js-id", options).then(container => {
+	console.log("callback - tsparticles config loaded");
+}).catch(error => {
+	console.error(error);
+});
+
+
+
+let h1 = document.getElementById('main-header').children[0];
+h1.innerHTML = 'Celebrating the <emph>Winter Solstice</emph>';
+
+
+
+
+var observer = new IntersectionObserver(onIntersection, {
+	root: null,
+	threshold: .7
+})
+
+function onIntersection(entries, opts) {
+	entries.forEach(entry =>  {
+		const id = entry.target.getAttribute('id');
+		if (entry.isIntersecting) {
+			document.querySelector(`#main-nav>ul>li>a[href="#${id}"]`).parentElement.classList.add('active');
+		} else {
+			document.querySelector(`#main-nav>ul>li>a[href="#${id}"]`).parentElement.classList.remove('active');
+		}
+	})
+}
+
+// Use the observer to observe an element
+document.querySelectorAll('section').forEach((el) => observer.observe(el))
+
