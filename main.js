004975fa987a9014daae2852c75b905a82584036
Plutonium-239 <plutonium.239.811@gmail.com>
2024-12-28 12:12:41 +0530

    remove unneeded tsparticles settings to save space

--- main.js
+++ main.js
@@ -10,18 +10,6 @@ const options = {
     "size": "",
     "opacity": 1
   },
-  "backgroundMask": {
-    "composite": "destination-out",
-    "cover": {
-      "opacity": 1,
-      "color": {
-        "value": ""
-      }
-    },
-    "enable": false
-  },
-  "clear": true,
-  "defaultThemes": {},
   "delay": 0,
   "fullScreen": {
     "enable": true,
@@ -30,67 +18,9 @@ const options = {
   "detectRetina": true,
   "duration": 0,
   "fpsLimit": 120,
-  "manualParticles": [],
   "particles": {
-    "bounce": {
-      "horizontal": {
-        "value": 1
-      },
-      "vertical": {
-        "value": 1
-      }
-    },
-    "collisions": {
-      "absorb": {
-        "speed": 2
-      },
-      "bounce": {
-        "horizontal": {
-          "value": 1
-        },
-        "vertical": {
-          "value": 1
-        }
-      },
-      "enable": false,
-      "maxSpeed": 50,
-      "mode": "bounce",
-      "overlap": {
-        "enable": true,
-        "retries": 0
-      }
-    },
     "color": {
       "value": "#888",
-      "animation": {
-        "h": {
-          "count": 0,
-          "enable": false,
-          "speed": 1,
-          "decay": 0,
-          "delay": 0,
-          "sync": true,
-          "offset": 0
-        },
-        "s": {
-          "count": 0,
-          "enable": false,
-          "speed": 1,
-          "decay": 0,
-          "delay": 0,
-          "sync": true,
-          "offset": 0
-        },
-        "l": {
-          "count": 0,
-          "enable": false,
-          "speed": 1,
-          "decay": 0,
-          "delay": 0,
-          "sync": true,
-          "offset": 0
-        }
-      }
     },
     "effect": {
       "close": true,
@@ -98,81 +28,18 @@ const options = {
       "options": {},
       "type": []
     },
-    "groups": {},
     "move": {
-      "angle": {
-        "offset": 0,
-        "value": 90
-      },
-      "attract": {
-        "distance": 200,
-        "enable": false,
-        "rotate": {
-          "x": 3000,
-          "y": 3000
-        }
-      },
-      "center": {
-        "x": 50,
-        "y": 50,
-        "mode": "percent",
-        "radius": 0
-      },
-      "decay": 0,
-      "distance": {},
-      "direction": "none",
-      "drift": 0,
       "enable": true,
-      "gravity": {
-        "acceleration": 9.81,
-        "enable": false,
-        "inverse": false,
-        "maxSpeed": 50
-      },
-      "path": {
-        "clamp": false,
-        "delay": {
-          "value": 0
-        },
-        "enable": false,
-        "options": {}
-      },
-      "outModes": {
-        "default": "out",
-        "bottom": "out",
-        "left": "out",
-        "right": "out",
-        "top": "out"
-      },
       "random": false,
       "size": true,
       "speed": 0.2,
-      "spin": {
-        "acceleration": 0,
-        "enable": false
-      },
       "straight": false,
-      "trail": {
-        "enable": false,
-        "length": 11,
-        "fill": {
-          "color": {
-            "value": ""
-          }
-        }
-      },
-      "vibrate": false,
-      "warp": false
     },
     "number": {
       "density": {
         "enable": true,
         "width": 1920,
         "height": 1080
-      },
-      "limit": {
-        "mode": "delete",
-        "value": 0
       },
       "value": 300
     },
@@ -193,18 +60,6 @@ const options = {
         "destroy": "none"
       }
     },
-    "reduceDuplicates": false,
-    "shadow": {
-      "blur": 0,
-      "color": {
-        "value": "#000"
-      },
-      "enable": false,
-      "offset": {
-        "x": 0,
-        "y": 0
-      }
-    },
     "shape": {
       "close": true,
       "fill": true,
@@ -213,20 +68,6 @@ const options = {
     },
     "size": {
       "value": 2,
-      "animation": {
-        "count": 0,
-        "enable": false,
-        "speed": 5,
-        "decay": 0,
-        "delay": 0,
-        "sync": false,
-        "mode": "auto",
-        "startValue": "random",
-        "destroy": "none"
-      }
-    },
-    "stroke": {
-      "width": 0
     },
     "zIndex": {
       "value": 0,
@@ -234,150 +75,11 @@ const options = {
       "sizeRate": 1,
       "velocityRate": 1
     },
-    "destroy": {
-      "bounds": {},
-      "mode": "none",
-      "split": {
-        "count": 1,
-        "factor": {
-          "value": 3
-        },
-        "rate": {
-          "value": {
-            "min": 4,
-            "max": 9
-          }
-        },
-        "sizeOffset": true,
-        "particles": {}
-      }
-    },
-    "roll": {
-      "darken": {
-        "enable": false,
-        "value": 0
-      },
-      "enable": false,
-      "enlighten": {
-        "enable": false,
-        "value": 0
-      },
-      "mode": "vertical",
-      "speed": 25
-    },
-    "tilt": {
-      "value": 0,
-      "animation": {
-        "enable": false,
-        "speed": 0,
-        "decay": 0,
-        "sync": false
-      },
-      "direction": "clockwise",
-      "enable": false
-    },
-    "twinkle": {
-      "lines": {
-        "enable": false,
-        "frequency": 0.05,
-        "opacity": 1
-      },
-      "particles": {
-        "enable": false,
-        "frequency": 0.05,
-        "opacity": 1,
-        "color": {
-          "value": ""
-        }
-      }
-    },
-    "wobble": {
-      "distance": 5,
-      "enable": false,
-      "speed": {
-        "angle": 50,
-        "move": 10
-      }
-    },
-    "life": {
-      "count": 0,
-      "delay": {
-        "value": 0,
-        "sync": false
-      },
-      "duration": {
-        "value": 0,
-        "sync": false
-      }
-    },
-    "rotate": {
-      "value": 0,
-      "animation": {
-        "enable": false,
-        "speed": 0,
-        "decay": 0,
-        "sync": false
-      },
-      "direction": "clockwise",
-      "path": false
-    },
-    "orbit": {
-      "animation": {
-        "count": 0,
-        "enable": false,
-        "speed": 1,
-        "decay": 0,
-        "delay": 0,
-        "sync": false
-      },
-      "enable": false,
-      "opacity": 1,
-      "rotation": {
-        "value": 45
-      },
-      "width": 1
-    },
-    "links": {
-      "blink": false,
-      "color": {
-        "value": "#fff"
-      },
-      "consent": false,
-      "distance": 100,
-      "enable": false,
-      "frequency": 1,
-      "opacity": 1,
-      "shadow": {
-        "blur": 5,
-        "color": {
-          "value": "#000"
-        },
-        "enable": false
-      },
-      "triangles": {
-        "enable": false,
-        "frequency": 1
-      },
-      "width": 1,
-      "warp": false
-    },
-    "repulse": {
-      "value": 0,
-      "enabled": false,
-      "distance": 1,
-      "duration": 1,
-      "factor": 1,
-      "speed": 1
-    }
   },
   "pauseOnBlur": true,
   "pauseOnOutsideViewport": true,
-  "responsive": [],
   "smooth": false,
-  "style": {},
-  "themes": [],
   "zLayers": 100,
-  "emitters": [],
   "motion": {
     "disable": false,
     "reduce": {
